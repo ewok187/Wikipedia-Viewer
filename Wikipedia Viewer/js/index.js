f6312904dbc0752655da6736a42801e158ff589f
ewok187 <k.wessalowski@gmx.de>
2017-12-13 23:28:45 +0100

    fixing style problems & bugs

--- Wikipedia Viewer/js/index.js
+++ Wikipedia Viewer/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#search").click(function() {
     var searchTerm = $("#searchTerm").val();
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
-    
+
     $.ajax({
       type: "GET",
       url: url,
@@ -12,7 +12,7 @@ $(document).ready(function() {
        // console.log(data[1][0]); heading
        // console.log(data[2][0]); desc
        // console.log(data[3][0]); link
-        $("output").empty();
+        $("#output").empty();
       for(var i=0; i<data[1].length;i++)  {
         $("#output").append("<li><a href= "+data[3][i]+">"+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
       }
