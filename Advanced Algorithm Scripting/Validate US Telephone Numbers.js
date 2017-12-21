6c9c448397be22f150b97cbb4c96e7349ac22a38
Konrad Wessalowski <k.wessalowski@gmx.de>
2017-12-21 18:13:38 +0100

    Delete Validate US Telephone Numbers.js

--- Advanced Algorithm Scripting/Validate US Telephone Numbers.js
+++
@@ -1,6 +0,0 @@
-function telephoneCheck(str) {
-  var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
-  return regex.test(str);
-}
-
-// create regex to check if str got the right format 
